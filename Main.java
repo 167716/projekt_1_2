ea9f60829e0a52dbd6649f3d98ac0a7c0c723283
167716 <101186863+167716@users.noreply.github.com>
2022-03-22 11:45:09 +0000

    ostateczny

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Wypisywanie liczb");
+    int a=5;
+    double b=8;
+    char c='c';
+    String d="Patryk";
+    
+     System.out.println("a= "+a+" \nb=" +b+ "\nc=" +c+"\nd="+d);
   }
 }
